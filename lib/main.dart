7415c90d51b2ae5122b8daac75f29000e7f88f81
子午 <wanglei@cai-inc.com>
2023-08-28 11:28:09 +0800

    【update】

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int rightNumber = 1;
+
+  void changeNumber() {
+    setState(() {
+      leftNumber = Random.secure().nextInt(6) + 1;
+      rightNumber = Random.secure().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            child: Image.asset("images/dice$leftNumber.png"),
+            onPressed: () {
+              changeNumber();
+            },
+          )),
+          Expanded(
+              child: TextButton(
+            child: Image.asset("images/dice$rightNumber.png"),
+            onPressed: () {
+              changeNumber();
+            },
+          )),
+        ],
+      ),
+    );
   }
 }
